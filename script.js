53ef9b6fa2e7ee6798696e72f94ad46187e2ca00
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-24 14:46:59 +0700

    Write operate()

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 
 
+function operate(a, operator, b) {
+  switch (operator) {
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return add(a, b);
+  }
+}
 
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
