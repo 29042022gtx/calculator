6ad32f76f81fc7e941136f82f03278831329067e
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-25 10:41:48 +0700

    Add keyboard support

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let calculator = document.querySelector('#calculator');
 calculator.addEventListener('click', clickCalculator);
 let display = document.querySelector('#display');
+display.addEventListener('keyup', typeIn)
 let a = 0;
 let b = 0;
 let operator = null
-
 
 function clickCalculator(event = new Event()) {
   switch (event.target.id) {
@@ -21,47 +21,78 @@ function clickCalculator(event = new Event()) {
     case 'negative':
       populate('-');
       break;
+    case 'point':
+      addPoint();
+      break;
     case 'operator':
-      b = parseFloat(display.textContent) || 0;
-      if (operator == null)
-        a = b
-      else
-        a = round(operate(a, operator, b), 5);
-      console.warn(operator + b);
-      console.log(a);
-      clearDisplay();
-      operator = event.target.textContent;
+      pressOperator(event.target.textContent);
       break;
     case 'operate':
-      b = parseFloat(display.textContent) || 0;
-      if (operator == null)
-        a = b
-      else
-        a = round(operate(a, operator, b), 5);
-
-      console.warn(operator + b);
-      console.log(a);
-
-      clearDisplay();
-      populate('' + a)
-      operator = null;
+      pressOperate();
       break;
     default: 
       populate(event.target.textContent)
   }
 }
 
+function typeIn(event) {
+  if (event.key == 'Shift')
+    return;
+  console.log(event.key);
+  let operatorList = '+-*/%'
+  if (operatorList.includes(event.key)) {
+    pressOperator(event.key);
+  } else if (event.key == 'Enter' || event.key == '=') {
+    pressOperate();
+  } else if (event.key == 'Escape') {
+    clearAll();
+  }
+}
+
+function pressOperate() {
+  if (process())
+        populate(a)
+      operator = null;
+}
+
+function pressOperator(key) {
+  process();
+  operator = key;
+}
+
+function process() {
+  b = parseFloat(getDisplayContent()) || 0;
+
+  if (operator == '/' && b == 0) {
+    clearAll();
+    populate('Divide by 0!');
+    return false;
+  }
+
+  if (operator == null)
+    a = b
+  else
+    a = round(operate(a, operator, b), 5);
+
+  console.warn(operator + b);
+  console.log(a);
+
+  clearDisplay();
+  return true;
+}
+
+function getDisplayContent() {
+  return display.value
+}
+
+function addPoint() {
+  if (!getDisplayContent().includes('.'))
+    populate('.')
+}
+
 function round(n, d) {
   d = 10**d
   return Math.round(n * d) / d
-}
-
-function deleteCharacter() {
-  display.textContent = display.textContent.slice(0, -1)
-}
-
-function clearDisplay() {
-  display.textContent = ''
 }
 
 function clearAll() {
@@ -71,8 +102,16 @@ function clearAll() {
   clearDisplay()
 }
 
+function deleteCharacter() {
+  display.value = getDisplayContent().slice(0, -1)
+}
+
 function populate(val) {
-  display.textContent += val;
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = ''
 }
 
 function operate(a, operator, b) {
